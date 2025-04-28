d98312dd41f9f0c8486128e4b4ead09d940f425b
Ethan Chaney <chaneyea@mail.uc.edu>
2025-04-28 12:16:52 -0400

    Test

--- Lula_Final_Project/index.aspx
+++ Lula_Final_Project/index.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Lula_Final_Project.index" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Final_Project.Index" %>
 
 <!DOCTYPE html>
 
@@ -9,6 +9,24 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <!-- Ethan's Problem 42 Section Below -->
+        <asp:Button ID="btnShowSolution" runat="server" Text="Problem 42" OnClick="btnShowSolution_Click" />
+        <br />
+
+        <asp:Label ID="lblDescription" runat="server" Font-Bold="true" Text=""></asp:Label>
+        <br />
+
+        <asp:Label ID="lblTestCase1" runat="server" Text=""></asp:Label>
+        <br />
+        <asp:Label ID="lblResult1" runat="server" Text=""></asp:Label>
+        <br />
+
+        <asp:Label ID="lblTestCase2" runat="server" Text=""></asp:Label>
+        <br />
+        <asp:Label ID="lblResult2" runat="server" Text=""></asp:Label>
+        </div>
+        <div>
+
         </div>
     </form>
 </body>
--- Lula_Final_Project/index.aspx.cs
+++ Lula_Final_Project/index.aspx.cs
@@ -4,14 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Lulu_Final_Project;
 
-namespace Lula_Final_Project
+namespace Final_Project
 {
-    public partial class index : System.Web.UI.Page
+    public partial class Index : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        /* Ethan's problem 42 below */
+        protected void btnShowSolution_Click(object sender, EventArgs e)
+        {
+            var problem = new LeetsCodeProblem42();
+
+            int[] test1 = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            int[] test2 = { 4, 2, 0, 3, 2, 5 };
+
+            lblDescription.Text = problem.GetDescription();
+            lblTestCase1.Text = "Test Case 1: [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]";
+            lblResult1.Text = "Output: " + problem.Solve(test1);
+            lblTestCase2.Text = "Test Case 2: [4, 2, 0, 3, 2, 5]";
+            lblResult2.Text = "Output: " + problem.Solve(test2);
         }
     }
 }
